2e6515099190e882074335f0e13394c38aecfbcb
Shadab834 <70321669+Shadab834@users.noreply.github.com>
2022-11-05 21:40:17 +0530

    Add files via upload

---
+++ BubbleSortDemo.java
@@ -0,0 +1,17 @@
+class BubbleSortDemo{
+    public static void main(String[] args){
+        int arr[]= {18,1,20,54,65,8,3,7,9};
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+                
+            }
+            System.out.println(arr[i]);
+        }
+    }
+}
